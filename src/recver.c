e7957d6295c0eda3c196386d5ca0e88eb682d4d0
Draal <julien.d.a@live.fr>
2020-11-09 17:49:10 +0100

    gros commit - echange serveur/client complet

--- src/recver.c
+++ src/recver.c
@@ -12,18 +12,9 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 
-//serveur racine
-
-void arg_check(int argc, char *argv);
-void domtab_create();
-void domaine_create(char *racine, char *addr, int port);
-char *resolution(char *buff);
-void tab_null();
-int search_domain(char *str);
-void send_back(struct sockaddr_in6 client, char *message);
-
 #define STR_SIZE 2048
 #define CHECK(op) do { if (op == -1) rerror(#op);} while(0)
+#define CHECK_NOER(op, erno) do { if (op == -1) return(erno);} while(0)
 
 noreturn void rerror(char *str) {
 	if (errno != 0) {
@@ -35,128 +26,182 @@ noreturn void rerror(char *str) {
     exit(EXIT_FAILURE);
 }
 
-typedef struct {
-	char *racine;
-	char *addr;
-	int port;
-} domaine;
+int serv_type = 0;
+//0: non renseigné 1: serveur racine
+//2: serveur de nom de rang 1 3: serveur de nom de rang 2
 
-domaine *dom_tab[50];
-int lport = 3500;	//entre 1024 et 5000
+void arg_check(int argc, char *argv[]);
+noreturn void usage(char *str);
+int resolution(char *search_file, char *buff, int sockfd, struct sockaddr_in6 client);
+int msg_builder(char **old, char *recu, char *code, char *line);
+char *racine_extractor(char *buff);
 
-int main() {
+// ./recver 3500 bddserv1 1
+
+int main(int argc, char *argv[]) {
+	arg_check(argc, argv);
+	serv_type = atoi(argv[3]);
+
+//
+//	Configure IPV4 & IPV6 listener
+//
+
 	int sockfd;
 	char buff[STR_SIZE];
 	socklen_t addrlen;
 	struct sockaddr_in6 my_addr;
 	struct sockaddr_in6 client;
+	addrlen = sizeof(struct sockaddr_in6);
 	
 	CHECK((sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)));
 	my_addr.sin6_family = AF_INET6;
-	my_addr.sin6_port = htons(lport);
-	CHECK(inet_pton(AF_INET6, "::1", &my_addr.sin6_addr.s6_addr));
-	addrlen = sizeof(struct sockaddr_in6);
+	my_addr.sin6_port = htons(atoi(argv[1]));
+	my_addr.sin6_addr = in6addr_any;
+	
+	CHECK(setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &(int){0}, sizeof(int)));	//On désactive ipv6 only
+	//CHECK(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)));	//On permet de réutiliser la socket
 	CHECK(bind(sockfd, (struct sockaddr *) &my_addr, addrlen));
 	
-	domtab_create();
-	
-	char *msg;
 	while(1) {
 		memset(buff, '\0',STR_SIZE);
-		
-		CHECK(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &client, &addrlen));	//en faire un thread ou un processus pour continuer a écouter meme si on est en plein traitement voire faire plusieurs ecoutes simultanement
-		
-		if((msg = resolution(buff)) == NULL); //renvoyer un message d'erreur
-		else {
-			printf("%s\n", msg);
-			send_back(client, msg);
-			free(msg);
-		}
+		CHECK(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &client, &addrlen));	//ecoutes simulannees ?
+		printf("recu : %s", buff);
+		if (resolution(argv[2], buff, sockfd, client) != 0) return 1;
 	}
 	
 	close(sockfd);	//pas besoin de check dans tous les cas on quitte ensuite.
-	
     return 0;
 }
 
-char *resolution(char *buff) {
-	int end = strlen(buff), i = end, j = 0, id;
-	char racine[STR_SIZE], *address, temp[10];
-	while (i >= 0 && buff[i] != '.') { i--;	}
-	i++;
-	for (;i < end; i++) {
-		racine[j] = buff[i];
+int resolution(char *search_file, char *buff, int sockfd, struct sockaddr_in6 client) {
+	char *message, *racine, *line;
+	FILE *fd;
+	
+	racine = racine_extractor(buff);
+	
+	if ((message = malloc(STR_SIZE)) == NULL) return -1;
+	memset(message, '\0',STR_SIZE);
+	
+	if ((fd = fopen(search_file, "r")) == NULL) return -1;
+	if ((line = malloc(STR_SIZE)) == NULL) return -1;
+	memset(line, '\0',STR_SIZE);
+	while (getline(&line, &(size_t){STR_SIZE}, fd) > 0) {
+		if (strncmp(racine, line, strlen(racine)) == 0) {
+			if (msg_builder(&message, buff, "1", line) == -1) rerror("msg builder");	//remplir les args
+		}
+	}
+	fclose(fd);
+	free(racine);
+	free(line);
+	
+	if (message[0] == '\0') {
+		if (msg_builder(&message, buff, "-1", NULL) == -1) rerror("msg builder");
+	}
+	
+	printf("repondu : %s\n", message);
+	message[strlen(message)-1] = '\0';
+	CHECK_NOER(sendto(sockfd, message, strlen(message)+1, 0, (struct sockaddr *) &client, sizeof(struct sockaddr_in6)), -1);
+	
+	free(message);
+	return 0;
+}
+
+int msg_builder(char **old, char *recu, char *code, char *line) {
+	int rt;
+	char *racine, *address, *port, *saveptr, *line_cpy;
+	if (line != NULL) { //on sort racine, port et adresse de la ligne lue
+		if ((line_cpy = malloc(STR_SIZE)) == NULL) rerror("malloc");
+		memset(line_cpy, '\0',STR_SIZE);
+		strcpy(line_cpy, line);	//no overflow since they have the same length
+	
+		racine = strtok_r(line_cpy, " | ", &saveptr);
+		address = strtok_r(NULL, " | ", &saveptr);
+		port = strtok_r(NULL, " | ", &saveptr);
+	
+		if (*old[0] == '\0') {
+			rt = snprintf(*old, STR_SIZE, "%s |%s| %s, %s, %s", recu, code, racine, address, port);
+			if (rt < 0 || rt >= STR_SIZE) return -1;
+		} else {
+			if (strlen(*old)+strlen(racine)+strlen(address)+strlen(port)+8 > STR_SIZE) return -1;
+			strncat(*old, " | ", STR_SIZE-strlen(*old));
+			strncat(*old, racine, STR_SIZE-strlen(*old));
+			strncat(*old, ", ", STR_SIZE-strlen(*old));
+			strncat(*old, address, STR_SIZE-strlen(*old));
+			strncat(*old, ", ", STR_SIZE-strlen(*old));
+			strncat(*old, port, STR_SIZE-strlen(*old));
+		}
+		free(line_cpy);
+	} else {
+		if (*old[0] == '\0') {
+			rt = snprintf(*old, STR_SIZE, "%s |%s|", recu, code);
+			if (rt < 0 || rt >= STR_SIZE) return -1;
+		} else {
+			if (strlen(*old)+6 > STR_SIZE) return -1;
+			strncat(*old, " |", STR_SIZE-strlen(*old));
+			strncat(*old, code, STR_SIZE-strlen(*old));
+			strncat(*old, "|", STR_SIZE-strlen(*old));
+		}
+	}
+	return 0;
+}
+
+char *racine_extractor(char *buff) {
+	int i = 0, j = 0, end;
+	char *saveptr, *buff_cpy, *racine, *site;
+	
+	if ((racine = malloc(STR_SIZE)) == NULL) rerror("malloc");
+	if ((buff_cpy = malloc(STR_SIZE)) == NULL) rerror("malloc");
+	strcpy(buff_cpy, buff);	//no overflow since they have the same length
+	
+	site = strtok_r(buff_cpy, " | ", &saveptr); //on balance l'id
+	site = strtok_r(NULL, " | ", &saveptr);	//on balance l'horodatage
+	site = strtok_r(NULL, " | ", &saveptr);
+	
+	end = strlen(site);
+	
+	if (serv_type < 3) {
+		while (i < end && site[i] != '.') { i++; }
+		i++;
+	}
+	if (serv_type < 2) {
+		while (i < end && site[i] != '.') { i++; }
+	}
+	for (; i < end && site[i] != '\n'; i++) {
+		racine[j] = site[i];
 		j++;
 	}
 	racine[j] = '\0';
-	if ((id = search_domain(racine)) == -1) return NULL;
 	
-	if ((address = malloc(STR_SIZE * sizeof(*address))) == NULL)
-		rerror("address malloc");
-	address[0] = '\0';
-	strcat(address, dom_tab[id]->addr);
-	strcat(address, " | ");
-	sprintf(temp, "%d", dom_tab[id]->port);
-	strcat(address, temp);
-	return address;
+	free(buff_cpy);
+	return racine;
 }
 
-void send_back(struct sockaddr_in6 client, char *message) {
-	int sockfd;
-	socklen_t addrlen;
-	//struct sockaddr_in dest;
-	char adr[STR_SIZE];
-	/*strcpy(str, ret_adrs);
-	adrs = strtok_r(str, " | ", &saveptr);
-	port = strtok_r(NULL, " | ", &saveptr);*/
+void arg_check(int argc, char *argv[]) {
+	if (argc != 4) {
+		usage("bad number of argument (4)\n\n");
+	}
+	int tmp;
+	tmp = atoi(argv[1]);
+	if (tmp < 1024 || tmp > 65535) {
+		usage("error argv[1] : port en dehors de la plage [1024,65535]\n\n");
+	}
 	
-	CHECK((sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)));
-	//dest.sin_family = AF_INET;
-	//dest.sin_port = htons(atoi(port));
-	addrlen = sizeof(struct sockaddr_in6);
-	//CHECK(inet_pton(AF_INET, adrs, &dest.sin_addr));
-	CHECK(sendto(sockfd, message, strlen(message), 0, (struct sockaddr *) &client, addrlen));
-	printf("IP address is: %s\n", inet_ntop(AF_INET6, &client.sin6_addr, adr, STR_SIZE));
-	printf("port is: %d\n", (int) ntohs(client.sin6_port));
+	struct stat s;	
+	CHECK(stat(argv[2], &s));
+	if (!S_ISREG(s.st_mode)) {
+		usage("error argv[2] : not a regular file\n\n");
+	}
 	
-}
-
-int search_domain(char *str) {
-	int i = 0;
-	while (i < 50 && dom_tab[i] != NULL && strcmp(str, dom_tab[i]->racine)) { i++; }
-	if (i < 50 && dom_tab[i] != NULL && !strcmp(str, dom_tab[i]->racine)) { return i; }
-	return -1;
-}
-
-void domtab_create() {
-	tab_null();
-	domaine_create("fr", "::1", 3501);
-	domaine_create("com", "::1", 3502);
-	domaine_create("eu", "::1", 3503);
-	domaine_create("net", "::1", 3504);
-	domaine_create("org", "::1", 3505);
-}
-
-void domaine_create(char *racine, char *addr, int port) {
-	int i = 0;
-	while(i < 50 && dom_tab[i] != NULL) i++;
-	if (i != 50) {
-		if ((dom_tab[i] = malloc(sizeof(domaine))) == NULL)
-			rerror("domaine malloc");
-		if ((dom_tab[i]->addr = malloc(STR_SIZE)) == NULL)
-			rerror("address malloc");
-		if ((dom_tab[i]->racine = malloc(20)) == NULL)
-			rerror("racine malloc");
-		strcpy(dom_tab[i]->racine, racine);
-		strcpy(dom_tab[i]->addr, addr);
-		dom_tab[i]->port = port;
+	tmp = atoi(argv[3]);
+	if (tmp < 1 || tmp > 3) {
+		usage("error argv[3] : not in values {1,2,3}\n\n");
 	}
 }
 
-void tab_null() {
-	int i;
-	for(i = 0; i < 50; i++) {
-		dom_tab[i] = NULL;
-	}
+noreturn void usage(char *str) {
+	char str2[] = "./recver port bddserv1 type\nport est le port du serveur\nbddserv1 est un fichier qui contient des lignes (ex: '.fr | 127.0.0.1 | 3501'). Il s'agit des adresses des serveurs de nom inférieurs\ntype correspond a la profondeur du serveur\n";
+	write(STDERR_FILENO, str, strlen(str));
+	write(STDERR_FILENO, str2, strlen(str2));
+	exit(EXIT_FAILURE);
 }
 
--- src/sender.c
+++ src/sender.c
@@ -11,10 +11,12 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
+#include <sys/time.h>
 
 void arg_check(int argc, char *argv[]);
 
 #define CHECK(op) do { if (op == -1) rerror(#op);} while(0)
+#define CHECK_NOER(op, erno) do { if (op == -1) return(erno);} while(0)
 #define STR_SIZE 4096
 
 noreturn void rerror(char *str) {
@@ -27,38 +29,150 @@ noreturn void rerror(char *str) {
     exit(EXIT_FAILURE);
 }
 
-// ./sender ::1 3500 "unistra.fr"
-// l'adresse peut-etre passee avec des "" ou sans
+// ./sender bddclient bddsites
 
-void recevoir();
+void arg_check(int argc, char *argv[]);
+noreturn void usage(char *str);
+int choix_serv(FILE *fd, char **buff);
+int sender(char *site, char *buff);
+char *msg_builder(char *site);
 
 int main(int argc, char *argv[]) {
-	if (argc != 4) rerror("bad number of arguments");
+	arg_check(argc, argv);
+	size_t n = STR_SIZE;
+	char *buff, *site;
+	if ((buff = malloc(STR_SIZE)) == NULL) rerror("malloc buff");
+	if ((site = malloc(STR_SIZE)) == NULL) rerror("malloc buff");
+	FILE *fd, *fe;
+	fd = fopen(argv[1], "r");
+	fe = fopen(argv[2], "r");
+	
+	getline(&buff, &n, fd);
+	while (getline(&site, &n, fe) > 0) {
+		
+		if (site[0] != '\0' && site[0] != '\n') {
+			int a;
+			if((a = sender(site, buff)) != 0)
+				return a;
+		}
+	}
+	if (errno != 0) rerror("read");
+	
+    return 0;
+}
+
+int sender(char *site, char *adrs_port) {
 	int sockfd;
-	char buff[STR_SIZE];
+	char buff[STR_SIZE], copy[STR_SIZE], *message, *adrs, *port, *saveptr;
 	socklen_t addrlen;
 	struct sockaddr_in6 dest;
 	struct sockaddr_in6 retour;
 	
-	CHECK((sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)));
+	strcpy(copy, adrs_port);
+	adrs = strtok_r(copy, " | ", &saveptr);
+	port = strtok_r(NULL, " | ", &saveptr);
+	
+	message = msg_builder(site);
+	
+	CHECK_NOER((sockfd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)), 1);
 	dest.sin6_family = AF_INET6;
-	dest.sin6_port = htons(atoi(argv[2]));
+	dest.sin6_port = htons(atoi(port));
+	dest.sin6_addr = in6addr_any;
+	CHECK(setsockopt(sockfd, IPPROTO_IPV6, IPV6_V6ONLY, &(int){0}, sizeof(int)));
 	
-	/*printf("port : %i\n", getsockname(sockfd, (struct sockaddr *) &dest, &addrlen));*/
 	addrlen = sizeof(struct sockaddr_in6);
-	CHECK(inet_pton(AF_INET6, argv[1], &dest.sin6_addr));
-	CHECK(sendto(sockfd, argv[3], strlen(argv[3]), 0, (struct sockaddr *) &dest, addrlen));
+	CHECK_NOER(inet_pton(AF_INET6, adrs, &dest.sin6_addr), 2);
+	message[strlen(message)-1] = '\0';
+	printf("message : %s\n", message);
+	CHECK_NOER(sendto(sockfd, message, strlen(message)+1, 0, (struct sockaddr *) &dest, addrlen), 3);
 	
-	CHECK(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &retour, &addrlen));
+	
+	CHECK_NOER(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &retour, &addrlen), 4);
 	printf("buff : %s\n", buff);
-    return 0;
+	
+	int i;
+	char *tmp, *saveptr2;
+	strcpy(copy, buff);
+	adrs = strtok_r(copy, "|", &saveptr);
+	for (i = 0; i < 3; i++)
+		adrs = strtok_r(NULL, "|", &saveptr);
+	if (strlen(adrs) == 2 && adrs[0] == '-' && adrs[1] == '1')
+		return -1;
+	while ((tmp = strtok_r(NULL, "|", &saveptr)) != NULL) {
+		strcpy(copy, tmp);
+		adrs = strtok_r(copy, ", ", &saveptr2);
+		adrs = strtok_r(NULL, ", ", &saveptr2);
+		port = strtok_r(NULL, ", ", &saveptr2);
+		
+		dest.sin6_port = htons(atoi(port));
+		//normallement on ferait la meme chose pour l'addresse
+		CHECK_NOER(sendto(sockfd, message, strlen(message)+1, 0, (struct sockaddr *) &dest, addrlen), 3);
+		CHECK_NOER(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &retour, &addrlen), 4);
+	}
+	printf("reponse : %s\n", buff);
+	
+	strcpy(copy, buff);
+	adrs = strtok_r(copy, "|", &saveptr);
+	for (i = 0; i < 3; i++)
+		adrs = strtok_r(NULL, "|", &saveptr);
+	if (strlen(adrs) == 2 && adrs[0] == '-' && adrs[1] == '1')
+		return -1;
+	while ((tmp = strtok_r(NULL, "|", &saveptr)) != NULL) {
+		strcpy(copy, tmp);
+		adrs = strtok_r(copy, ", ", &saveptr2);
+		adrs = strtok_r(NULL, ", ", &saveptr2);
+		port = strtok_r(NULL, ", ", &saveptr2);
+		
+		dest.sin6_port = htons(atoi(port));
+		//normallement on ferait la meme chose pour l'addresse
+		CHECK_NOER(sendto(sockfd, message, strlen(message)+1, 0, (struct sockaddr *) &dest, addrlen), 3);
+		CHECK_NOER(recvfrom(sockfd, buff, STR_SIZE, 0, (struct sockaddr *) &retour, &addrlen), 4);
+	}
+	printf("reponse : %s\n", buff);
+	
+	free(message);
+	return 0;
 }
 
-/*void arg_check(int argc, char *argv[]) {
-	if (argc != 4) rerror("bad number of arguments");
-	int i, nbPoints = 0, end = strlen(argv[i]);
-	for (i = 0; i < end; i++) {
-		if (argv[i] == '.') nbPoints++;
+char *msg_builder(char *site) {
+	static long int trans_id = 1;
+	unsigned long long int tim;
+	int rt;
+	char *msg;
+	struct timeval horodatage;
+	
+	if ((msg = malloc(STR_SIZE)) == NULL) return NULL;
+	memset(msg, '\0',STR_SIZE);
+	CHECK_NOER(gettimeofday(&horodatage, NULL), NULL);
+	tim = horodatage.tv_sec*1000 + horodatage.tv_usec/1000;
+	
+	rt = snprintf(msg, STR_SIZE, "%ld | %llu | %s", trans_id, tim, site);
+	
+	if (rt < 0 || rt >= STR_SIZE) return NULL;
+	return msg;
+}
+
+void arg_check(int argc, char *argv[]) {
+	if (argc != 3) {
+		usage("bad number of argument (3)\n\n");
 	}
-	if (i == end) rerror("not an adress");
-}*/
+		
+	struct stat s;	
+	CHECK(stat(argv[1], &s));
+	if (!S_ISREG(s.st_mode)) {
+		usage("error argv[1] : not a regular file\n\n");
+	}
+	
+	CHECK(stat(argv[2], &s));
+	if (!S_ISREG(s.st_mode)) {
+		usage("error argv[2] : not a regular file\n\n");
+	}
+}
+
+noreturn void usage(char *str) {
+	char str2[] = "./sender bddclient bddsites\n\nbddclient est un fichier qui contient des lignes (ex: '127.0.0.1 | 3500'). Il s'agit de l'adresse (IPV4 ou IPV6) et du numéro de port des serveurs racines\nbddsites est un fichier qui contient des lignes (ex: 'www.unistra.fr'). Il s'agit des noms a résoudre\n";
+	write(STDERR_FILENO, str, strlen(str));
+	write(STDERR_FILENO, str2, strlen(str2));
+	exit(EXIT_FAILURE);
+}
+
